f628e58bdb44d63a209445d82529a425dd905183
shekhar <shekharpa@cybage.com>
2020-11-04 19:31:43 +0530

    login

--- registration/worshop.js
+++ registration/worshop.js
@@ -1,12 +1,17 @@
 
 
 $(document).ready(()=>{
-    $('header').load('header.html .anchor,.navbar',function(){
+    //load header
+    // $('header').load('header.html .anchor,.navbar',function(){
        
-    })
+    // })
+
+    //load footer
     $('footer').load('footer.html ',function(){
       
     })
+
+    //password checking
     $('#pass,#cpass').on('keyup',function()
     {
     if($('#pass').val() === $('#cpass').val())
@@ -22,23 +27,27 @@ $(document).ready(()=>{
     }
 });
 
-$('#dat').on('change',function()
-{
-    var dat=$('#dat').val();
-    var today=new Date();
-    var dob=new Date(dat);
-    if(today > dob)
-    {
-        $('#but').removeAttr("disabled");
-    }
-    else if(today == dob) {
-        $('#but').attr("disabled","true");
-    }
-    else  {
-        $('#but').attr("disabled","true");
-    }
-})
 
+// //date checking
+// $('#dat').on('change',function()
+// {
+//     var dat=$('#dat').val();
+//     var today=new Date();
+//     var dob=new Date(dat);
+//     // if(today.toString()== dob.toString()) {
+//     //     $('#but').attr("disabled","true");
+//     // }
+//     if(today > dob)
+//     {
+//         $('#but').removeAttr("disabled");
+//     }
+  
+//     else  {
+//         $('#but').attr("disabled","true");
+//     }
+// })
+
+//gender validation
 
 $('#but,.gen').on('click',function(){
     var radio=$('.gen');
@@ -63,18 +72,87 @@ $('#but,.gen').on('click',function(){
 
 
 
+//submit user
+// $('#but').on('click',function(){
+//     let name=$('#name').val();
+//     let email=$('#email').val();
+//     let password=$('#pass').val();
+//     let cpassword=$('#cpass').val();
+//     // let dob=$('#dat').val();
+//     let phone=$('#phone').val();
+//     let gender=$('input[name="gender"]:checked').val();
+  
+
+//     var user={"name":name,"email":email,"password":password,"cpassword":cpassword,"phone":phone,
+//      "gender":gender};
+    
+//     $.ajax({
+//         type: "POST",
+//         url: "http://localhost:3000/users",
+//         data: JSON.stringify(user),
+//         success: function(data, status, xhr){
+//             console.log('success'+status);
+//             alert('success'+status);
+            
+//         },
+        
+//         error:function(jqXhr, textStatus, errorMessage){
+//             console.log('error'+errorMessage);
+//         },
+//         dataType: "text",
+//         contentType : "application/json",
+        
+//       });
+
+
+$('form').submit((a)=>{
+    a.preventDefault();
+    let name=$('#name').val();
+    let email=$('#email').val();
+    let password=$('#pass').val();
+    // let cpassword=$('#cpass').val();
+    // let dob=$('#dat').val();
+    let phone=$('#phone').val();
+    let gender=$('input[name="gender"]:checked').val();
+  
+
+    var user={"name":name,"email":email,"password":password,"phone":phone,
+     "gender":gender};
+    
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/users",
+        data: JSON.stringify(user),
+        success: function(data, status, xhr){
+            console.log('success'+status);
+            alert('success'+status);
+            
+        },
+        
+        error:function(jqXhr, textStatus, errorMessage){
+            console.log('error'+errorMessage);
+        },
+        dataType: "text",
+        contentType : "application/json",
+        
+      });
+    
+    
 
 
 })
+$('.button1').click(function(){
+    console.log('reach');
+    $('#myForm').css('display','none');
+    $('#myForm1').css('display','block');
+    $('footer').addClass('foot2');
+})
 
-function myFunction() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-       
-        x.removeAttribute("style");
-      
-    } else {
-      x.style.display = "block";
-    }
-  
-  }
+$('.button2').click(function(){
+    console.log('reach');
+    $('#myForm1').css('display','none');
+    $('#myForm').css('display','block');
+    $('footer').removeClass('foot2');
+})
+
+})
